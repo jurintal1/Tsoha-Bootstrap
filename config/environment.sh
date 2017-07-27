2cf714b29de2a9db331631cd5d1a9c1addf10ec6
hiihtoliitto <jussi.rintala@gmail.com>
2017-07-27 10:21:47 +0300

    sovellus alustettu, versionhallinta käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+
+USERNAME="jurintal"
+PROJECT_FOLDER="drinkit"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
