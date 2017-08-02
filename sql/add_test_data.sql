376d323bf21aa0bdf2420a1ebe96cbed683c127c
hiihtoliitto <jussi.rintala@gmail.com>
2017-08-02 19:49:38 +0300

    taulut luotu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,7 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (nimi, salasana, oikeustaso) VALUES ('Jussi', 'kaakkuri', '0');
+INSERT INTO Resepti (laatija, nimi, lisaysaika) 
+	VALUES ((SELECT id FROM Kayttaja WHERE nimi = 'Jussi'), 'Pelkkä piimä', now());
+INSERT INTO Ainesosa (nimi)  VALUES ('piimä');
+INSERT INTO ReseptiAines (resepti_id, ainesosa_id)
+	VALUES ((SELECT id FROM Resepti WHERE nimi = 'Pelkkä piimä'),
+			(SELECT id FROM Ainesosa WHERE nimi = 'piimä'));
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,26 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) UNIQUE NOT NULL,
+salasana varchar(50) UNIQUE NOT NULL,
+oikeustaso integer NOT NULL CHECK(oikeustaso < 2)
+);
+
+CREATE TABLE Resepti(
+id SERIAL PRIMARY KEY,
+laatija integer REFERENCES Kayttaja(id),
+nimi varchar(50) UNIQUE NOT NULL,
+lisaysaika timestamp NOT NULL,
+ohje varchar(1000),
+lasi varchar(50),
+valmistustapa varchar(50)
+);
+
+CREATE TABLE Ainesosa(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE ReseptiAines(
+resepti_id integer REFERENCES Resepti(id),
+ainesosa_id integer REFERENCES Ainesosa(id)
+);
